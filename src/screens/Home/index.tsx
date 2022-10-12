8db2500da2c859a09e898563a2e453762416898f
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-10-12 10:15:03 -0300

    Criando a aplicacao

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { 
+  Container,
+  Title,
+} from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>
+        HOME
+      </Title>
+    </Container>
+  );
+}
+
+export default Home;
