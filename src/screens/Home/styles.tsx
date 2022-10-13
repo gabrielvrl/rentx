a39fdc6033581269828ff7dcb9e193b439ecb75e
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-10-13 07:20:38 -0300

    Tema Global da Aplicacao

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -4,6 +4,10 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+`;
