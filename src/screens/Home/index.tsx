bfbe54abfe9645061a10d90f1cc3e7bf92684176
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-10-15 12:57:39 -0300

    Criacao do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg'
 
+import { Car } from '../../components/Car';
+
 import { 
   Container,
   Header,
@@ -12,6 +14,26 @@ import {
 } from './styles';
 
 const Home: React.FC = () => {
+  const carDataOne = {
+    brand: 'AUDI',
+    name: 'RS 5 Coupe',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png',
+  };
+
+  const carDataTwo = {
+    brand: 'PORSCHE',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 340,
+    },
+    thumbnail: 'https://www.pngplay.com/wp-content/uploads/13/Porsche-Panamera-Transparent-File.png',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -31,6 +53,9 @@ const Home: React.FC = () => {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
