963170b1773247383abaf99a65154e2826f8a010
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-12-15 21:21:06 -0300

    Transições

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Button, StyleSheet, Dimensions } from 'react-native';
 
-import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
 
 import * as S from './styles';
 
+const WIDTH = Dimensions.get('window').width
 
 export function Splash() {
   const animation = useSharedValue(0);
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [ { translateX: animation.value } ]
+      transform: [
+        { 
+          translateX: withTiming(animation.value,{
+            duration: 1000,
+            easing: Easing.bezier(.01,1.07,1,-0.28)
+          })
+        }
+      ]
     }
   })
 
   const handleAnimationPosition = () => {
-    animation.value = Math.random() * 100;
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
   return (
